b42fcf517ca0e43a165527b9d07ad66b2c3560a2
dobbysfree <samashe.chang@gmail.com>
2026-09-20 23:16:44 -0400

    docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/wwwroot',
+    // Docker copies the client artifact from /app/client/dist into server/wwwroot.
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
